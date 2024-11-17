5b7d1b4ca8bf6ab8a008ae054c8a90388429b792
babblet <frk.haha@hotmail.se>
2024-11-17 19:25:26 +0100

    [Refactor] Move whitespace striping to a function

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,9 +1,14 @@
 export class PalindromeChecker {
+  private static removeWhitespace(input: string): string {
+    return input.replace(/ /g, "");
+  }
+
   public static isItAPalindrome(input: string): boolean {
     const reverse = input.split("").reverse().join("");
     return (
-      input.toLowerCase().replace(/ /g, "") ===
-      reverse.toLowerCase().replace(/ /g, "")
+      this.removeWhitespace(input.toLowerCase()) ===
+      this.removeWhitespace(reverse.toLowerCase())
     );
   }
 }
+
