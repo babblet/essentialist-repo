ae6da1be376209d771eb35294a72ae928037eaa1
babblet <frk.haha@hotmail.se>
2024-11-17 13:09:58 +0100

    [Green] Added Grading Checklist tests

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -7,6 +7,8 @@ describe("fizzbuzz", () => {
 
   it("should return 'Fizz' for multiples of 3", () => {
     expect(fizzbuzz(3)).toBe("Fizz");
+    expect(fizzbuzz(9)).toBe("Fizz");
+    expect(fizzbuzz(42)).toBe("Fizz");
   });
 
   it("should return 'Buzz' for multiples of 5", () => {
@@ -15,15 +17,23 @@ describe("fizzbuzz", () => {
 
   it("should return n as a string for numbers that are not multiples of 3 or 5", () => {
     expect(fizzbuzz(1)).toBe("1");
+    expect(fizzbuzz(43)).toBe("43");
+
   });
 
   it("should return 'FizzBuzz' for multiples of 3 and 5", () => {
     expect(fizzbuzz(15)).toBe("FizzBuzz");
+    expect(fizzbuzz(45)).toBe("FizzBuzz");
   });
 
   it("should only allow numbers from 1-100 as input", () => {
+    expect(() => fizzbuzz(-12)).toThrow();
     expect(() => fizzbuzz(0)).toThrow();
     expect(() => fizzbuzz(101)).toThrow();
+    expect(() => fizzbuzz(102)).toThrow();
+
+    expect(() => fizzbuzz("" as unknown as number)).toThrow();
+
     expect(() => fizzbuzz(50)).not.toThrow();
   });
 });
