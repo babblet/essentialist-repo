d6a9294d8c96a5388b73404ec4b0d1b40abc1861
babblet <frk.haha@hotmail.se>
2024-11-17 13:09:58 +0100

    [Red] Added test - it should return 'Fizz' for multiples of 3

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -4,4 +4,8 @@ describe("fizzbuzz", () => {
   it("should return a string", () => {
     expect(typeof fizzbuzz(1)).toBe("string");
   });
+
+  it("should return 'Fizz' for multiples of 3", () => {
+    expect(fizzbuzz(3)).toBe("Fizz");
+  });
 });
