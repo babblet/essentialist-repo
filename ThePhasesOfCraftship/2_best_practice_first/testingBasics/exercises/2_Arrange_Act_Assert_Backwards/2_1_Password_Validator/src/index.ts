3473be06813b7dcbe48812cb73a872cc87fa2379
babblet <frk.haha@hotmail.se>
2024-11-19 19:12:06 +0100

    [Arch] Add wishful implementation as documentation

---
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
@@ -0,0 +1,30 @@
+type PasswordValidationResult = {
+  result: boolean,
+  errors: PasswordValidationError[]
+}
+
+enum PasswordValidationError {
+  Length = "Password must be between 5 and 15 characters long",
+  Digit = "Password must contain at least one digit",
+  UpperCase = "Password must contain at least one upper case letter"
+}
+
+/**
+ * A utility class for validating passwords.
+ *
+ * This class provides a single static method to validate passwords
+ * against the following criteria:
+ * 
+ * - Password must be between 5 and 15 characters long
+ * - Password must contain at least one digit
+ * - Password must contain at least one upper case letter
+ *
+ * @example Usage of `PasswordValidator`:
+ * ```typescript
+ * const password: string = "Password123";
+ * const validationResult: PasswordValidationResult = PasswordValidator.validate(password);
+ * console.log(validationResult.result); // true
+ * console.log(validationResult.errors); // []
+ * ```
+ */
+class PasswordValidator {}
