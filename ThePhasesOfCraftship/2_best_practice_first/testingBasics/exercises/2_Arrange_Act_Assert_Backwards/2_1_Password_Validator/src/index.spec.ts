cc46c1238099f7be95f5d306ed21beba22f58911
babblet <frk.haha@hotmail.se>
2024-11-19 20:42:02 +0100

    [Refactor] Move valid passwords to its own tests

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -1,14 +1,18 @@
 import { PasswordValidationError, PasswordValidator } from '.'
 
 describe('Password Validator', () => {
-  describe('Password has to be between 5 and 15 characters long', () => {
-    it('knows that "Test1" is valid', () => {
-      const password = "Test1";
-      const result = PasswordValidator.validate(password);
-      expect(result.result).toBeTruthy();
-      expect(result.errors).toHaveLength(0);
-    })
+  it.each([
+    "Test1",
+    "Test4Pass",
+    "someThing4u",
+    "thisIsGreat4me",
+  ])('knows that "%s" is valid', (password) => {
+    const result = PasswordValidator.validate(password);
+    expect(result.result).toBeTruthy();
+    expect(result.errors).toHaveLength(0);
+  })
 
+  describe('Password has to be between 5 and 15 characters long', () => {
     it('knows that "Pa55" has invalid length', () => {
       const password = "Pa55";
       const result = PasswordValidator.validate(password);
@@ -25,13 +29,6 @@ describe('Password Validator', () => {
   })
 
   describe('Contains at least one digit', () => {
-    it('knows that "Test4Pass" is valid', () => {
-      const password = "Test4Pass";
-      const result = PasswordValidator.validate(password);
-      expect(result.result).toBeTruthy();
-      expect(result.errors).toHaveLength(0);
-    })
-
     it('knows that "TeStInG" has missing digit', () => {
       const password = "TeStInG";
       const result = PasswordValidator.validate(password);
@@ -41,13 +38,6 @@ describe('Password Validator', () => {
   })
 
   describe('Contains at least one upper case letter', () => {
-    it('knows that "someThing4u" is valid', () => {
-      const password = "someThing4u";
-      const result = PasswordValidator.validate(password);
-      expect(result.result).toBeTruthy();
-      expect(result.errors).toHaveLength(0);
-    })
-
     it('knows that "password123" has missing upper case letter', () => {
       const password = "password123";
       const result = PasswordValidator.validate(password);
