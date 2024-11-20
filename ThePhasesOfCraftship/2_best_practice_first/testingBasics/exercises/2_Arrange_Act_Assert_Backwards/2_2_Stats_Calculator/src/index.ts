3765c2c6fd95b9b6502e2911e3c846e98e0c7c13
babblet <frk.haha@hotmail.se>
2024-11-20 19:08:36 +0100

    [Arch] Create wishful implementation in docs

---
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.ts
@@ -0,0 +1,22 @@
+type StatsResult = {
+  min: number,
+  max: number,
+  count: number,
+  avg: number
+}
+
+
+/**
+ * A utility class for calculating stats from Integers.
+ *
+ * This class provides a single static method to calculate the minimum, maximum,
+ * number of elements, and average, from a sequence of integers.
+ *
+ * @example Usage of `PasswordValidator`:
+ * ```typescript
+ * const input: number[] = [1,2,3,4,5];
+ * const result: StatsResult = StatsCalculator.calculate(input);
+ * console.log(result); // { min: 1, max: 5, count: 5, avg: 3 }
+ * ```
+ */
+class StatsCalculator {}
