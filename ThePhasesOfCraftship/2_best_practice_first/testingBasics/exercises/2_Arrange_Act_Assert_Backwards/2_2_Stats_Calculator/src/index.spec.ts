15498386e16bfce7dfb253b7bbf2cd387da692dd
babblet <frk.haha@hotmail.se>
2024-11-20 19:49:27 +0100

    [Green] It is able to calculate the correct min value for input with both positive and negative values

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -18,4 +18,10 @@ describe('stats calculator', () => {
     const result = StatsCalculator.calculate(input);
     expect(result.min).toBe(-5);
   })
+
+  it('should calculate the correct min value for [1,-2,3,-4,5]', () => {
+    const input = [1, -2, 3, -4, 5];
+    const result = StatsCalculator.calculate(input);
+    expect(result.min).toBe(-4);
+  })
 })
