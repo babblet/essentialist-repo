b0f851d1aa6cdc240292a3341c0acba042429f3b
babblet <frk.haha@hotmail.se>
2024-11-21 15:12:05 +0100

    [Refactor] Remove first check for empty string

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -10,15 +10,14 @@
  */
 export class MilitartTimeValidator {
   public static validate(militaryTime: string): boolean {
-    if (militaryTime === '') return false;
-
     const [leftTime, rightTime] = militaryTime.split(' - ');
     if (!leftTime || !rightTime) return false;
 
-    if(leftTime.length !== 5 || rightTime.length !== 5) return false;
+    if (leftTime.length !== 5 || rightTime.length !== 5) return false;
     
     const [leftHour, leftMinute] = leftTime.split(':').map(Number);
     const [rightHour, rightMinute] = rightTime.split(':').map(Number);
+
     if (!leftHour || !leftMinute || !rightHour || !rightMinute) return false;
     if (leftHour > 23 || leftHour < 0 || rightHour > 23 || rightHour < 0) return false;
     if (leftMinute > 59 || leftMinute < 0 || rightMinute > 59 || rightMinute < 0) return false;
