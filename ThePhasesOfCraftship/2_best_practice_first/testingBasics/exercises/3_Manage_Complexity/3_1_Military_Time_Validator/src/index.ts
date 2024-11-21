493ecb8e9710ec30efe4f55cb3315390d7424cfd
babblet <frk.haha@hotmail.se>
2024-11-21 14:00:19 +0100

    [Arch] Create wishful implementation in class documentation

---
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -0,0 +1,11 @@
+/**
+ * @class MilitartTimeValidator
+ * @description
+ * This class is responsible for validating military time.
+ * 
+ * @example
+ * const militaryTime = "20:00 - 24:00";
+ * const isValid = MilitartTimeValidator.validate(militaryTime);
+ * console.log(isValid); // true
+ */
+class MilitartTimeValidator {}
