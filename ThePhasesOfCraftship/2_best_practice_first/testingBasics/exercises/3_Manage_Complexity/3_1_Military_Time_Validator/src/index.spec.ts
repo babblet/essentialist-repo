b1a3c279d04f93c816ba204214e0ad9770d3d590
babblet <frk.haha@hotmail.se>
2024-11-21 14:57:04 +0100

    [Refactor] Move invalid time checks to a it.each iteration

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,23 +1,16 @@
 import { MilitartTimeValidator } from './index';
 
 describe('military time validator', () => {
-  it('knows that an empty string is an invalid military time', () => {
-    const militaryTime = '';
+  it.each([
+    '',
+    '1:12 - 14:32',
+    '01:12 - 1:32'
+  ])('knows that %s is an invalid military time', (militaryTime) => {
     expect(MilitartTimeValidator.validate(militaryTime)).toBe(false)
   })
 
   it('knows that "01:12 - 14:32" is a valid military time', () => {
     const militaryTime = '01:12 - 14:32';
     expect(MilitartTimeValidator.validate(militaryTime)).toBe(true)
-  })
-
-  it('knows that "1:12 - 14:32" is an invalid military time', () => {
-    const militaryTime = '1:12 - 14:32';
-    expect(MilitartTimeValidator.validate(militaryTime)).toBe(false)
-  })
-
-  it('knows that "01:12 - 1:32" is an invalid military time', () => {
-    const militaryTime = '01:12 - 1:32';
-    expect(MilitartTimeValidator.validate(militaryTime)).toBe(false)
   })
 })
