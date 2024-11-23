2a8eb74703bdc706906f27e4b8efd33e3f3af42c
babblet <frk.haha@hotmail.se>
2024-11-23 18:34:48 +0100

    [Refactor] Move get classes assignments to ClassesController

--- ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/controllers/ClassesController.ts
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/controllers/ClassesController.ts
@@ -1,18 +1,16 @@
 import { Request, Response } from "express";
-import { Errors, isMissingKeys, parseForResponse } from "../shared";
+import { Errors, isMissingKeys, isUUID, parseForResponse } from "../shared";
 import { prisma } from "../database";
 
 export class ClassesController {
   static async create(req: Request, res: Response) {
     try {
       if (isMissingKeys(req.body, ["name"])) {
-        return res
-          .status(400)
-          .json({
-            error: Errors.ValidationError,
-            data: undefined,
-            success: false,
-          });
+        return res.status(400).json({
+          error: Errors.ValidationError,
+          data: undefined,
+          success: false,
+        });
       }
 
       const { name } = req.body;
@@ -26,6 +24,60 @@ export class ClassesController {
       res
         .status(201)
         .json({ error: undefined, data: parseForResponse(cls), success: true });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: Errors.ServerError, data: undefined, success: false });
+    }
+  }
+
+  static async readAssignments(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      if (!isUUID(id)) {
+        return res
+          .status(400)
+          .json({
+            error: Errors.ValidationError,
+            data: undefined,
+            success: false,
+          });
+      }
+
+      // check if class exists
+      const cls = await prisma.class.findUnique({
+        where: {
+          id,
+        },
+      });
+
+      if (!cls) {
+        return res
+          .status(404)
+          .json({
+            error: Errors.ClassNotFound,
+            data: undefined,
+            success: false,
+          });
+      }
+
+      const assignments = await prisma.assignment.findMany({
+        where: {
+          classId: id,
+        },
+        include: {
+          class: true,
+          studentTasks: true,
+        },
+      });
+
+      res
+        .status(200)
+        .json({
+          error: undefined,
+          data: parseForResponse(assignments),
+          success: true,
+        });
     } catch (error) {
       res
         .status(500)
--- ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/index.ts
@@ -18,6 +18,7 @@ app.get('/student/:id/assignments', StudentController.readAssignments);
 app.get('/student/:id/grades', StudentController.readGrades);
 
 app.post('/classes', ClassesController.create)
+app.get('/classes/:id/assignments', ClassesController.readAssignments)
 
 // POST student assigned to class
 app.post('/class-enrollments', async (req: Request, res: Response) => {
@@ -249,44 +250,6 @@ app.get('/assignments/:id', async (req: Request, res: Response) => {
         res.status(500).json({ error: Errors.ServerError, data: undefined, success: false });
     }
 });
-
-// GET all assignments for class
-app.get('/classes/:id/assignments', async (req: Request, res: Response) => {
-    try {
-        const { id } = req.params;
-        if(!isUUID(id)) {
-            return res.status(400).json({ error: Errors.ValidationError, data: undefined, success: false });
-        }
-
-        // check if class exists
-        const cls = await prisma.class.findUnique({
-            where: {
-                id
-            }
-        });
-
-        if (!cls) {
-            return res.status(404).json({ error: Errors.ClassNotFound, data: undefined, success: false });
-        }
-
-        const assignments = await prisma.assignment.findMany({
-            where: {
-                classId: id
-            },
-            include: {
-                class: true,
-                studentTasks: true
-            }
-        });
-    
-        res.status(200).json({ error: undefined, data: parseForResponse(assignments), success: true });
-    } catch (error) {
-        res.status(500).json({ error: Errors.ServerError, data: undefined, success: false });
-    }
-});
-
-// GET all student grades
-
 
 const port = process.env.PORT ?? 3000;
 
