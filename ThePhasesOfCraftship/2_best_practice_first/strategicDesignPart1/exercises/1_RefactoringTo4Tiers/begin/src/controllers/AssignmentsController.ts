c16ce4d65882667f567a3da6f84c156183894a70
babblet <frk.haha@hotmail.se>
2024-11-23 18:41:51 +0100

    [Refactor] Move get assignments to AssigmentsController

--- ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/controllers/AssignmentsController.ts
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/controllers/AssignmentsController.ts
@@ -1,18 +1,16 @@
 import { Request, Response } from "express";
-import { Errors, isMissingKeys, parseForResponse } from "../shared";
+import { Errors, isMissingKeys, isUUID, parseForResponse } from "../shared";
 import { prisma } from "../database";
 
 export class AssignmentsController {
   static async create(req: Request, res: Response) {
     try {
       if (isMissingKeys(req.body, ["classId", "title"])) {
-        return res
-          .status(400)
-          .json({
-            error: Errors.ValidationError,
-            data: undefined,
-            success: false,
-          });
+        return res.status(400).json({
+          error: Errors.ValidationError,
+          data: undefined,
+          success: false,
+        });
       }
 
       const { classId, title } = req.body;
@@ -24,8 +22,51 @@ export class AssignmentsController {
         },
       });
 
+      res.status(201).json({
+        error: undefined,
+        data: parseForResponse(assignment),
+        success: true,
+      });
+    } catch (error) {
       res
-        .status(201)
+        .status(500)
+        .json({ error: Errors.ServerError, data: undefined, success: false });
+    }
+  }
+  static async read(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      if (!isUUID(id)) {
+        return res
+          .status(400)
+          .json({
+            error: Errors.ValidationError,
+            data: undefined,
+            success: false,
+          });
+      }
+      const assignment = await prisma.assignment.findUnique({
+        include: {
+          class: true,
+          studentTasks: true,
+        },
+        where: {
+          id,
+        },
+      });
+
+      if (!assignment) {
+        return res
+          .status(404)
+          .json({
+            error: Errors.AssignmentNotFound,
+            data: undefined,
+            success: false,
+          });
+      }
+
+      res
+        .status(200)
         .json({
           error: undefined,
           data: parseForResponse(assignment),
