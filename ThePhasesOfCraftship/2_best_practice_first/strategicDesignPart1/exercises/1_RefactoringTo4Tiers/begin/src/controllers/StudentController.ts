29a96220a1618b3beba735673b5513ab771925fc
babblet <frk.haha@hotmail.se>
2024-11-23 18:23:07 +0100

    [Refactor] Move get single student logic to StudentController

--- ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/controllers/StudentController.ts
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/controllers/StudentController.ts
@@ -1,18 +1,16 @@
 import { Request, Response } from "express";
 import { prisma } from "../database";
-import { Errors, isMissingKeys, parseForResponse } from "../shared";
+import { Errors, isMissingKeys, isUUID, parseForResponse } from "../shared";
 
 export class StudentController {
   static async create(req: Request, res: Response) {
     try {
       if (isMissingKeys(req.body, ["name"])) {
-        return res
-          .status(400)
-          .json({
-            error: Errors.ValidationError,
-            data: undefined,
-            success: false,
-          });
+        return res.status(400).json({
+          error: Errors.ValidationError,
+          data: undefined,
+          success: false,
+        });
       }
 
       const { name } = req.body;
@@ -23,8 +21,54 @@ export class StudentController {
         },
       });
 
+      res.status(201).json({
+        error: undefined,
+        data: parseForResponse(student),
+        success: true,
+      });
+    } catch (error) {
       res
-        .status(201)
+        .status(500)
+        .json({ error: Errors.ServerError, data: undefined, success: false });
+    }
+  }
+
+  // GET a student by id
+  static async read(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      if (!isUUID(id)) {
+        return res
+          .status(400)
+          .json({
+            error: Errors.ValidationError,
+            data: undefined,
+            success: false,
+          });
+      }
+      const student = await prisma.student.findUnique({
+        where: {
+          id,
+        },
+        include: {
+          classes: true,
+          assignments: true,
+          reportCards: true,
+        },
+      });
+
+      if (!student) {
+        return res
+          .status(404)
+          .json({
+            error: Errors.StudentNotFound,
+            data: undefined,
+            success: false,
+          });
+      }
+
+      res
+        .status(200)
         .json({
           error: undefined,
           data: parseForResponse(student),
@@ -37,7 +81,7 @@ export class StudentController {
     }
   }
 
-  static async getAll(req: Request, res: Response) {
+  static async readAll(req: Request, res: Response) {
     try {
       const students = await prisma.student.findMany({
         include: {
@@ -49,13 +93,11 @@ export class StudentController {
           name: "asc",
         },
       });
-      res
-        .status(200)
-        .json({
-          error: undefined,
-          data: parseForResponse(students),
-          success: true,
-        });
+      res.status(200).json({
+        error: undefined,
+        data: parseForResponse(students),
+        success: true,
+      });
     } catch (error) {
       res
         .status(500)
