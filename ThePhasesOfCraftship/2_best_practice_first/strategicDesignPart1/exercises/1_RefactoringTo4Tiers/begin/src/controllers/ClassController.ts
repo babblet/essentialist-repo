47bc13d2bd964a06dd977fd5b31186fe5146c34d
babblet <frk.haha@hotmail.se>
2024-11-23 18:18:40 +0100

    [Tidy] Format ClassController

--- ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/controllers/ClassController.ts
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/controllers/ClassController.ts
@@ -3,23 +3,33 @@ import { Errors, isMissingKeys, parseForResponse } from "../shared";
 import { prisma } from "../database";
 
 export class ClassController {
-    static async create(req: Request, res: Response) {
-        try {
-            if (isMissingKeys(req.body, ['name'])) {
-                return res.status(400).json({ error: Errors.ValidationError, data: undefined, success: false });
-            }
-    
-            const { name } = req.body;
-    
-            const cls = await prisma.class.create({
-                data: {
-                    name
-                }
-            });
-    
-            res.status(201).json({ error: undefined, data: parseForResponse(cls), success: true });
-        } catch (error) {
-            res.status(500).json({ error: Errors.ServerError, data: undefined, success: false });
-        }
+  static async create(req: Request, res: Response) {
+    try {
+      if (isMissingKeys(req.body, ["name"])) {
+        return res
+          .status(400)
+          .json({
+            error: Errors.ValidationError,
+            data: undefined,
+            success: false,
+          });
+      }
+
+      const { name } = req.body;
+
+      const cls = await prisma.class.create({
+        data: {
+          name,
+        },
+      });
+
+      res
+        .status(201)
+        .json({ error: undefined, data: parseForResponse(cls), success: true });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: Errors.ServerError, data: undefined, success: false });
     }
+  }
 }
